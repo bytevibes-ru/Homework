8ff007be37b1b5ab07e13d79114abf2fbd1ed492
Sergey <30sergey06@mail.ru>
2026-06-30 20:12:55 +0300

    Задача 3. Будева логика

---
+++ 6dfizdriver/app2.js
@@ -0,0 +1,18 @@
+// Задача 3.
+
+const hasLicence = true;
+const age = 18;
+const isDrunk = false;
+
+if (age >= 18 && hasLicence === true && isDrunk === false) {
+    console.log('Может');
+} else {
+    console.log('Не может');    
+}
+
+
+
+
+
+
+
