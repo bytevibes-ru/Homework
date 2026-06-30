d9adefdd005918d9cc2cc2f4f8410fd84035143a
Sergey <30sergey06@mail.ru>
2026-06-30 20:01:45 +0300

    Задача 2 Переводчик

---
+++ 5 Languages/app2.js
@@ -0,0 +1,25 @@
+
+// Задача 2.
+const lang = 'ru';
+switch (true) {
+    case lang === 'de': 
+    console.log('Gutten tag!');
+    break;
+    case lang === 'ru': 
+    console.log('Привет!');
+    break;
+    case lang === 'en': 
+    console.log('Hi!');
+    break;
+    default:
+    console.log('error')
+}
+
+
+
+
+
+
+
+
+
